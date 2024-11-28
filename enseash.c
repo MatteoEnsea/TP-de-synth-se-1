3c7493a2b94aa627cdc634e1a6bfe748a92bf51b
MatteoD <matt.doffe@gmail.com>
2024-11-28 12:00:14 +0100

    Q2
    La façon de finir l'entrée de l'utilisateur (Q1) a été réparée.

--- enseash.c
+++ enseash.c
@@ -1,6 +1,7 @@
 #include <string.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
 #define BUFFER_SIZE 128 
 
@@ -10,13 +11,38 @@ void print_welcome_message() {
 }
 
 void print_prompt() {
-    const char *prompt = "enseash % \n";
+    const char *prompt = "enseash % ";
     write(STDOUT_FILENO, prompt, strlen(prompt));
 }
 
 int main() {
     char buffer[BUFFER_SIZE];
+    ssize_t read_size;  
+
     print_welcome_message();
-    print_prompt();
+
+    while (1){
+        print_prompt();
+
+        read_size = read(STDIN_FILENO, buffer, BUFFER_SIZE - 1);
+
+        if (read_size > 0 && buffer[read_size - 1] == '\n') {//Stocks and finish correctly the user's entry to use it later
+            buffer[read_size - 1] = '\0';
+        } 
+
+        pid_t pid = fork();
+
+        if (pid > 0){ //If father process, wait for the child process to finish
+            int status;
+            wait(&status);
+        }else if (pid==0){ //If child process, execute the command
+            execlp(buffer,buffer,NULL);
+            perror("Erreur lors de l'execution de la commande");
+            _exit(1);
+        }else if (pid==-1){ //If error using fork
+            perror("Erreur lors du fork");
+            return 1;
+        }
+    }
     return 0;
 }
